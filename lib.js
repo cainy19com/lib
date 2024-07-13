72f0c5ce6397744204b78cefa6cae723acdfce17
Alex Potter <58665866+cainy19com@users.noreply.github.com>
2024-07-13 10:45:19 +0800

    Create lib.js

---
+++ lib.js
@@ -0,0 +1,54 @@
+Object.prototype.getKeyByValue = function (value) {
+  for (const key in this)
+    if (this[key] == value)
+      return key
+  return null
+}
+
+
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+const LOG = console.log.bind(console)
+
+
+const GV = {} // Global variables
+const GF = {} // Global functions
+const GC = {} // Global classes
+
+
+GF.range = n => {
+  return [...Array(n).keys()]
+}
+
+
+GF.fetch_ = (api_url, json_data) => {
+  return fetch(api_url, {
+    method: "POST",
+    body: JSON.stringify(json_data),
+  }).then(res => {
+    if (res.status === 200)
+      return res.json()
+    else
+      throw Error(res.statusText)
+  })
+}
+
+
+GF.url_search_params = () => {
+  return new URL(window.location.href).searchParams
+}
+
+
+GF.meta = (theme_color) => {
+  document.head.innerHTML = `
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="${theme_color}">
+  `
+}
+
+
+GF.is_iphone = () => {
+  return /iphone/i.test(window.navigator.userAgent)
+}
