87cf03c30a75afe033119248716f229433ccbf59
Alex Potter <58665866+cainy19com@users.noreply.github.com>
2024-07-13 10:59:05 +0800

    Create lib.php

---
+++ lib.php
@@ -0,0 +1,59 @@
+<?php
+define("PLZ_LOGIN",  "Please login first");
+
+define("INVALID_EMAIL",    "Invalid email");
+define("INVALID_PASSWORD", "Invalid password");
+define("INVALID_CODE",     "Invalid code");
+
+define("WRONG_PASSWORD", "Wrong password");
+define("WRONG_CODE",     "Wrong code");
+
+define("NO_USER",    "No user found");
+define("NO_RECORD",  "No record found");
+define("NO_SERVICE", "Service unavailable, please try again later");
+
+define("DB_CONN_FAILED", "DB connection failed");
+define("QUERY_FAILED",   "Query failed");
+
+
+function res(int $code, string $msg = "", string $data = "") {
+    $arr = array();
+
+    $arr["code"] = $code;
+    $arr["msg"] = $msg;
+    $arr["data"] = $data;
+
+    echo json_encode($arr);
+    exit();
+}
+
+
+function conn($db, $pass) {
+    $conn = new mysqli("localhost", "root", $pass, $db);
+    $conn->connect_error and res(1, DB_CONN_FAILED);
+    return $conn;
+}
+
+
+function is_logged_in() {
+    session_start();
+    return $_SESSION["id"] ? true : false;
+}
+
+
+function echo_pre($str) {
+    echo "<pre>" . $str . "</pre>";
+}
+
+
+function get_post() {
+  $json_str = file_get_contents("php://input");
+  $json_obj = json_decode($json_str);
+  $json_arr = json_decode($json_str, true);
+
+  foreach ($json_arr as $k => $v)
+    $json_arr[$k] = trim($v);
+
+  return $json_arr;
+}
+
