bd2982ea14f9e84bbfc661cbcf2b2e7e72c0b6bf
cainy19com <cainy19com@gmail.com>
2024-07-16 13:27:27 +0800

    a

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="theme-color" content="">
+  <title></title>
+  <link rel="icon" href="favicon.ico">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/app.css">
+  <script src="js/lib.js"></script>
+  <script src="js/app.js"></script>
+</head>
+
+<body>
