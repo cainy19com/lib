63c79f4e3c7f31d9dc5e2350e58802b20396c286
Alex Potter <58665866+cainy19com@users.noreply.github.com>
2024-07-30 14:08:50 +0800

    Update lib.js

--- lib.js
+++ lib.js
@@ -110,7 +110,7 @@ GF.tap_highlight = async (el, color="#eee", duration=100) => {
   await new Promise((res, rej) => {
     setTimeout(() => {
       // DOM update may be async, f like "alert" will pause the rm
-      canvas.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
       canvas.remove()
       setTimeout(() => res(), 0) // Ain't enough
     }, duration)
